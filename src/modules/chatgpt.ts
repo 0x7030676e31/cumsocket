651cf3c64de5ce3a1b5b9b341548f1873b63eab0
0x7030676e31 <p0gn10wn14@gmail.com>
2022-12-08 21:10:19 +0100

    ChatGpt multiple questions at the same time

--- src/modules/chatgpt.ts
+++ src/modules/chatgpt.ts
@@ -4,14 +4,20 @@ import { ChatGPTAPI } from "chatgpt";
 export default class ChatGPT {
   public readonly ctx!: Core;
   public readonly id: string = "chatgpt";
-  public readonly env: string[] = ["chatgpt_token"];
+  public readonly env: string[] = ["chatgpt_token", "chatgpt_timeout", "chatgpt_cooldown"];
   
   private mention!: string;
   private api!: ChatGPTAPI;
   private free: boolean = true;
+  private timeout!: number;
+  private cooldown!: number;
 
   public async load(ctx: Core): Promise<void> {
+    this.timeout = +process.env.chatgpt_timeout!;
+    this.cooldown = +process.env.chatgpt_cooldown!;
     this.mention = `<@${ctx.getSelfId()}>`;
+
+    // init chatgpt
     this.api = new ChatGPTAPI({
       sessionToken: process.env.chatgpt_token!
     });
@@ -20,21 +26,29 @@ export default class ChatGPT {
 
   @Core.listen("MESSAGE_CREATE")
   public async onMessage(msg: types.MESSAGE_CREATE): Promise<any> {
+    // check if message starts with mention
     if (!msg.content.startsWith(this.mention)) return;
 
+    // get content
     const content = msg.content.slice(this.mention.length).trim();
     if (!content) return;
     
+    // check if chatgpt is free
     if (!this.free) return this.ctx.api.messages.reactionAdd(msg.channel_id, msg.id, "💬");
     this.free = false;
 
+    // send waiting message
     const { id } = await this.ctx.api.messages.respond(msg.channel_id, msg.id, "📨 Waiting for ChatGPT response...").unwrap();
-    this.api.sendMessage(content, { timeoutMs: 30_000 }).then(async (content) => {
-      await this.ctx.api.messages.edit(msg.channel_id, id, { content: content }).get();
-      setTimeout(() => this.free = true, 3000);
-    }, async (err) => {
-      await this.ctx.api.messages.edit(msg.channel_id, id, { content: "⚠️ ChatGPT encountered an error: " + err.message }).get();
-      setTimeout(() => this.free = true, 3000);
-    });
+    
+    // ask chatgpt
+    setTimeout(() => this.free = true, this.cooldown);
+    this.api.sendMessage(content, { timeoutMs: this.timeout }).then(
+      content => this.edit(msg.channel_id, id, content),
+      err => this.edit(msg.channel_id, id, "⚠️ ChatGPT encountered an error: " + err.message),
+    );
+  }
+
+  private async edit(channel_id: string, id: string, content: string): Promise<void> {
+    this.ctx.api.messages.edit(channel_id, id, { content: content, allowed_mentions: { parse: ["everyone", "roles", "users"], replied_user: false }, });
   }
 }
