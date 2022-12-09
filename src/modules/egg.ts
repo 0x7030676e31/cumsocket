6c13115a149061cd0cd7c6f095afacf774eb5cdb
0x7030676e31 <p0gn10wn14@gmail.com>
2022-12-09 20:28:04 +0100

    node 19 and egg ai preps

--- src/modules/egg.ts
+++ src/modules/egg.ts
@@ -1,35 +1,59 @@
 import Core, { types } from "../core/index.js";
+// import { exec } from "child_process";
+// import zmq from 'zeromq';
 
 const EGGS = ["🥚", "🍳"];
 const REG = /egg|🥚|🍳/i; 
+const PORT = 5555;
+const ATT_REG = /^image(?!=\/gif)/;
 
 export default class Egg {
   public readonly ctx!: Core;
   public readonly id: string = "egg";
-  public readonly env: string[] = ["rare_egg_chance"];
+  public readonly env: string[] = ["rare_egg_chance", "egg_ai_threshold"];
   
   private self!: string;
   private chance!: number;
+  private threshold!: number;
+  // private client!: zmq.Request;
 
   public async load(ctx: Core): Promise<void> {
     this.self = ctx.getSelfId();
     this.chance = +process.env.rare_egg_chance!
+    this.threshold = +process.env.egg_ai_threshold!
+  
+    // run egg ai server
+    // exec("python3 ai/egg_detector.py");
+
+    // load zmq client
+    // this.client = new zmq.Request();
+    // this.client.connect(`tcp://localhost:${PORT}`);
   }
 
   @Core.listen("MESSAGE_CREATE")
-  public async onMessage(msg: types.MESSAGE_CREATE): Promise<void> {
-    if (msg.author.id === this.self || !REG.test(msg.content)) return;
+  public async onMessage(msg: types.MESSAGE_CREATE): Promise<any> {
+    if (msg.author.id === this.self) return;
     
     // add a random egg reaction
-    this.ctx.api.messages.reactionAdd(msg.channel_id, msg.id, this.getEgg());
+    if (REG.test(msg.content)) return this.ctx.api.messages.reactionAdd(msg.channel_id, msg.id, this.getEgg());
+
+    // get attachments and check if they contain an egg
+    const urls = msg.attachments.filter(v => v.content_type && ATT_REG.test(v.content_type)).map(v => v.url);
+    if (!urls.length) return;
+
+    const hasEgg = await this.hasEggAI(urls);
+    if (hasEgg) this.ctx.api.messages.reactionAdd(msg.channel_id, msg.id, this.getEgg());
   }
 
   @Core.listen("MESSAGE_UPDATE")
   public async onMessageUpdate(msg: types.MESSAGE_UPDATE): Promise<any> {
     if (msg.author?.id === this.self) return;
 
-    // check if the message has an egg reaction and if content matches egg pattern
-    const hasEggContent = REG.test(msg.content ?? "");
+    // get attachments
+    const urls = msg.attachments?.filter(v => v.content_type && ATT_REG.test(v.content_type)).map(v => v.url) || [];
+
+    // check if the message has an egg reaction and if content matches egg pattern or contains an egg in one of the attachments
+    const hasEggContent = REG.test(msg.content ?? "") || await this.hasEggAI(urls);
     const hasEggReaction = await this.hasEgg(msg.channel_id, msg.id);
   
     if (hasEggReaction === null) return;
@@ -54,5 +78,18 @@ export default class Egg {
   // generate a random egg
   private getEgg(): string {
     return EGGS[Math.random() > this.chance ? 0 : 1];
+  }
+
+  // check if an image contains an egg using the egg ai
+  private async hasEggAI(urls: string[]): Promise<boolean> {
+    return false;
+
+    // for (const url of urls) {
+    //   this.client.send(url);
+    //   const value = (await this.client.receive()).toString();
+    //   if (+value >= this.threshold) return true;
+    // }
+
+    // return false;
   }
 }
