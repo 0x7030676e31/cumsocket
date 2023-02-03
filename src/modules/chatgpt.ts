b19a1ac50949fb06f86db73ae6b20f104cf64857
0x7030676e31 <0x7030676e31@proton.me>
2023-02-03 21:24:38 +0100

    chatgpt alive (again)

--- src/modules/chatgpt.ts
+++ src/modules/chatgpt.ts
@@ -72,95 +72,95 @@ export default class ChatGPT {
     if (!content) return;
 
     // BRUH
-    this.ctx.api.messages.respond(msg.channel_id, msg.id, "💀 OpenAI broke chatgpt unofficial API again; R.I.P.", false);
+    // this.ctx.api.messages.respond(msg.channel_id, msg.id, "💀 OpenAI broke chatgpt unofficial API again; R.I.P.", false);
 
-    // // Check if user wants to restart converation
-    // if (RESTART.test(content)) {
-    //   delete this.converations[msg.author.id];
-    //   this.ctx.dbQuery("DELETE FROM chatgpt_conversations WHERE user_id = $1;", msg.author.id);
-    //   this.ctx.api.messages.reactionAdd(msg.channel_id, msg.id, "🔄");
-    //   return;
-    // }
+    // Check if user wants to restart converation
+    if (RESTART.test(content)) {
+      delete this.converations[msg.author.id];
+      this.ctx.dbQuery("DELETE FROM chatgpt_conversations WHERE user_id = $1;", msg.author.id);
+      this.ctx.api.messages.reactionAdd(msg.channel_id, msg.id, "🔄");
+      return;
+    }
 
-    // // Don't allow to process more than 3 messages at the same time
-    // if (this.activeConvos >= 3) return this.ctx.api.messages.reactionAdd(msg.channel_id, msg.id, "🚎") as any;
-    // this.activeConvos++;
+    // Don't allow to process more than 3 messages at the same time
+    if (this.activeConvos >= 3) return this.ctx.api.messages.reactionAdd(msg.channel_id, msg.id, "🚎") as any;
+    this.activeConvos++;
 
-    // // Send placeholder message
-    // const response = await this.ctx.api.messages.respond(msg.channel_id, msg.id, "📨 Waiting for ChatGPT response...", false).get();
-    // if (!response.ok) {
-    //   this.activeConvos--;
-    //   this.ctx.log("ChatGPT", `There was an error with responding to the message on channel ${msg.channel_id}`);
-    //   return;
-    // }
+    // Send placeholder message
+    const response = await this.ctx.api.messages.respond(msg.channel_id, msg.id, "📨 Waiting for ChatGPT response...", false).get();
+    if (!response.ok) {
+      this.activeConvos--;
+      this.ctx.log("ChatGPT", `There was an error with responding to the message on channel ${msg.channel_id}`);
+      return;
+    }
 
-    // // Fetch chatgpt message
-    // try {
-    //   var gptResponse = await this.api.sendMessage(content, {
-    //     ...(this.converations[msg.author.id] && {
-    //       conversationId: this.converations[msg.author.id].convID,
-    //       parentMessageId: this.converations[msg.author.id].messID,
-    //     }),
-    //     timeoutMs: this.timeout,
-    //   });
-    // } catch (e) {
-    //   // Update placeholder message
-    //   let { message, statusCode } = e as ChatGPTError;
-    //   const error = ERROR.exec(message);
-    //   if (error !== null) {
-    //     const errObject = JSON.parse(message.slice(error[0].length)).error as GPTError;
-    //     message = errObject.message;
-    //   }
+    // Fetch chatgpt message
+    try {
+      var gptResponse = await this.api.sendMessage(content, {
+        ...(this.converations[msg.author.id] && {
+          conversationId: this.converations[msg.author.id].convID,
+          parentMessageId: this.converations[msg.author.id].messID,
+        }),
+        timeoutMs: this.timeout,
+      });
+    } catch (e) {
+      // Update placeholder message
+      let { message, statusCode } = e as ChatGPTError;
+      const error = ERROR.exec(message);
+      if (error !== null) {
+        const errObject = JSON.parse(message.slice(error[0].length)).error as GPTError;
+        message = errObject.message;
+      }
       
-    //   this.ctx.api.messages.edit(msg.channel_id, response.data.id, {
-    //     content: `⚠️ There was an error with ChatGPT: ${message}${statusCode ? ` (code: ${statusCode})` : ""}`,
-    //     allowed_mentions: { parse: ["everyone", "roles", "users"], replied_user: false },
-    //   });
+      this.ctx.api.messages.edit(msg.channel_id, response.data.id, {
+        content: `⚠️ There was an error with ChatGPT: ${message}${statusCode ? ` (code: ${statusCode})` : ""}`,
+        allowed_mentions: { parse: ["everyone", "roles", "users"], replied_user: false },
+      });
 
-    //   this.activeConvos--;
-    //   return;
-    // }
+      this.activeConvos--;
+      return;
+    }
 
-    // // Check if response cointains some bad words etc and if message is too long to send normally
-    // const isBad = msg.guild_id === "391020510269669376" ? BAD_WORDS.test(gptResponse.text) || SCAM_PATTERN.test(gptResponse.text) : false;
-    // const tooLong = isBad || gptResponse.text.length > 2000;
+    // Check if response cointains some bad words etc and if message is too long to send normally
+    const isBad = msg.guild_id === "391020510269669376" ? BAD_WORDS.test(gptResponse.text) || SCAM_PATTERN.test(gptResponse.text) : false;
+    const tooLong = isBad || gptResponse.text.length > 2000;
 
-    // // Update converation data in the database
-    // if (this.converations[msg.author.id] === undefined) this.ctx.dbQuery("INSERT INTO chatgpt_conversations (user_id, conversation_id, message_id) VALUES ($1, '$2', '$3');", msg.author.id, gptResponse.conversationId!, gptResponse.id!);
-    // else this.ctx.dbQuery("UPDATE chatgpt_conversations SET conversation_id = '$1', message_id = '$2' WHERE user_id = $3;", gptResponse.conversationId!, gptResponse.id!, msg.author.id);
+    // Update converation data in the database
+    if (this.converations[msg.author.id] === undefined) this.ctx.dbQuery("INSERT INTO chatgpt_conversations (user_id, conversation_id, message_id) VALUES ($1, '$2', '$3');", msg.author.id, gptResponse.conversationId!, gptResponse.id!);
+    else this.ctx.dbQuery("UPDATE chatgpt_conversations SET conversation_id = '$1', message_id = '$2' WHERE user_id = $3;", gptResponse.conversationId!, gptResponse.id!, msg.author.id);
 
-    // // Update gpt_responses count, converation data and active converations
-    // this.ctx.storage?.numericIncr("gpt_responses");
-    // this.converations[msg.author.id] = {
-    //   convID: gptResponse.conversationId!,
-    //   messID: gptResponse.id!,
-    // }
-    // this.activeConvos--;
+    // Update gpt_responses count, converation data and active converations
+    this.ctx.storage?.numericIncr("gpt_responses");
+    this.converations[msg.author.id] = {
+      convID: gptResponse.conversationId!,
+      messID: gptResponse.id!,
+    }
+    this.activeConvos--;
 
-    // // Upload file if needed
-    // if (tooLong) var files = await this.ctx.api.messages.uplaodFiles(msg.channel_id, {
-    //   filename: "response.txt",
-    //   content: gptResponse.text,
-    // }).get();
+    // Upload file if needed
+    if (tooLong) var files = await this.ctx.api.messages.uplaodFiles(msg.channel_id, {
+      filename: "response.txt",
+      content: gptResponse.text,
+    }).get();
 
-    // // Check if there was an error with uploading file
-    // if (files!?.ok === false) {
-    //   this.ctx.api.messages.edit(msg.channel_id, response.data.id, {
-    //     content: "Response is too long to send normally and there was an error with uploading it as a file",
-    //     allowed_mentions: { parse: ["everyone", "roles", "users"], replied_user: false },
-    //   });
-    //   return;
-    // }
+    // Check if there was an error with uploading file
+    if (files!?.ok === false) {
+      this.ctx.api.messages.edit(msg.channel_id, response.data.id, {
+        content: "Response is too long to send normally and there was an error with uploading it as a file",
+        allowed_mentions: { parse: ["everyone", "roles", "users"], replied_user: false },
+      });
+      return;
+    }
 
-    // // Update placeholder message
-    // this.ctx.api.messages.edit(msg.channel_id, response.data.id, {
-    //   ...(!tooLong && { content: isBad ? "Chatgpt response contains some bad words which are unwelcome on this server <:flarogus:888054896769773600>" : gptResponse.text}),
-    //   ...(files! && { attachments: [{
-    //     id: files.data.attachments[0].id.toString(),
-    //     filename: "response.txt",
-    //     uploaded_filename: files.data.attachments[0].upload_filename,
-    //   }] }),
-    //   allowed_mentions: { parse: ["everyone", "roles", "users"], replied_user: false },
-    // });
+    // Update placeholder message
+    this.ctx.api.messages.edit(msg.channel_id, response.data.id, {
+      ...(!tooLong && { content: isBad ? "Chatgpt response contains some bad words which are unwelcome on this server <:flarogus:888054896769773600>" : gptResponse.text}),
+      ...(files! && { attachments: [{
+        id: files.data.attachments[0].id.toString(),
+        filename: "response.txt",
+        uploaded_filename: files.data.attachments[0].upload_filename,
+      }] }),
+      allowed_mentions: { parse: ["everyone", "roles", "users"], replied_user: false },
+    });
   }
 }
